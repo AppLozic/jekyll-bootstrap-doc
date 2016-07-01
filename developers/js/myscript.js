f8ad707eb9783c198b2eb96341439da2e9764805
sunip123 <sujit.singh@hotmail.com>
2016-07-01 16:57:52 +0530

    #1

---
+++ developers/js/myscript.js
@@ -0,0 +1,10 @@
+/*
+$(document).ready(function() {
+    $.getJSON("js/data.json", funtion(data){
+        $.each(data.person, function() {
+            
+            $("ul").append("<li>Name: "+this['name']+"</li><li>Age: "+this['name']+"<\li></br>");
+        });
+    });    
+  });
+ */
