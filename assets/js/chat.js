213d7dbe418b6d82c30c620e225b2180862ed62e
Anshul Garg <anshul.garg@mobitexter.net>
2016-09-27 13:04:21 +0530

    #983 initiate normal chat if topicId not passed

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,22 +1,22 @@
 var userId;
 
 $(document).ready(function () {
-       userId = (typeof localStorage !== 'undefined') ? localStorage.getItem('mckLoginId') : "";
-       if (!userId && $.cookie("mckUserId")) {
-           userId = $.cookie("mckUserId");
-       }
-       var $name = $("#userName");
-       var $email = $("#email");
-       var $guestLogin = $("#guestLogin");
-       var $password = $("#password");
-       var $companyName = $("#companyName");
-       var $chatLoginModal = $("#chatLoginModal");
-       var $form_chat_login = $("#form-chat-login");
-       var $error_chat_login = $("#error-chat-login");
-       var $submit_chat_login = $("#submit-chat-login");
-       var $chatLoginModalLink = $("[name='chatLoginModalLink']");
-       var welcomeMessage = "Welcome to Applozic, may I know your name and company name?";
-      var topics = {
+    userId = (typeof localStorage !== 'undefined') ? localStorage.getItem('mckLoginId') : "";
+    if (!userId && $.cookie("mckUserId")) {
+        userId = $.cookie("mckUserId");
+    }
+    var $name = $("#userName");
+    var $email = $("#email");
+    var $guestLogin = $("#guestLogin");
+    var $password = $("#password");
+    var $companyName = $("#companyName");
+    var $chatLoginModal = $("#chatLoginModal");
+    var $form_chat_login = $("#form-chat-login");
+    var $error_chat_login = $("#error-chat-login");
+    var $submit_chat_login = $("#submit-chat-login");
+    var $chatLoginModalLink = $("[name='chatLoginModalLink']");
+    var welcomeMessage = "Welcome to Applozic, may I know your name and company name?";
+    var topics = {
         'android':
                 {'title': 'Android', // Product title
                     'subtitle': 'Documentation', // Product subTitle or Product Id
@@ -70,178 +70,189 @@ $(document).ready(function () {
         }
     };
 
-       $("#form-chat-login input").on('click', function () {
-           $error_chat_login.removeClass('show').addClass('hide');
-       });
+    $("#form-chat-login input").on('click', function () {
+        $error_chat_login.removeClass('show').addClass('hide');
+    });
 
-       function initPlugin(userId, callback) {
-         (function(d, m){var s, h;
+    function initPlugin(userId, callback) {
+        (function (d, m) {
+            var s, h;
             s = document.createElement("script");
             s.type = "text/javascript";
-            s.async=true;
-            s.src="https://apps.applozic.com/sidebox.app";
-            h=document.getElementsByTagName('head')[0];
+            s.async = true;
+            s.src = "https://apps.applozic.com/sidebox.app";
+            h = document.getElementsByTagName('head')[0];
             h.appendChild(s);
-            window.applozic=m;
-            m.init=function(t){m._globals=t;}})(document, window.applozic || {});
+            window.applozic = m;
+            m.init = function (t) {
+                m._globals = t;
+            }
+        })(document, window.applozic || {});
 
-           window.applozic.init({
-               appId: "applozic-sample-app",
-               userId: userId,
-               notificationIconLink: "https://www.applozic.com/resources/images/applozic_icon.png",
-               desktopNotification: true,
-               contactDisplayName: displayName,
-               authenticationTypeId :1,
-               onInit: function (response) {
-                 scrollToHash();
-                 heap.identify(userId);
-                 $applozic.fn.applozic('loadContacts', {"contacts": [{"userId": "applozic", "displayName": "Applozic Support",
-                    "imageLink": "https://www.applozic.com/resources/images/applozic_icon.png"}
-                  ]});
-                   if (typeof callback === 'function') {
-                       callback(response);
-                   }
-               },
-               topicBox: false,
-               getTopicDetail: function(topicId) {
-                          return topics[topicId];
-                       }
-              });
-       }
-       if (userId) {
-           $chatLoginModalLink.addClass('hide');
-           initPlugin(userId);
-       }
+        window.applozic.init({
+            appId: "applozic-sample-app",
+            userId: userId,
+            notificationIconLink: "https://www.applozic.com/resources/images/applozic_icon.png",
+            desktopNotification: true,
+            contactDisplayName: displayName,
+            authenticationTypeId: 1,
+            onInit: function (response) {
+                scrollToHash();
+                heap.identify(userId);
+                $applozic.fn.applozic('loadContacts', {"contacts": [{"userId": "applozic", "displayName": "Applozic Support",
+                            "imageLink": "https://www.applozic.com/resources/images/applozic_icon.png"}
+                    ]});
+                if (typeof callback === 'function') {
+                    callback(response);
+                }
+            },
+            topicBox: false,
+            getTopicDetail: function (topicId) {
+                return topics[topicId];
+            }
+        });
+    }
+    if (userId) {
+        $chatLoginModalLink.addClass('hide');
+        initPlugin(userId);
+    }
 
-       $chatLoginModalLink.click(function (e) {
-           $('.modal').modal('hide');
-           $error_chat_login.html("");
-           $error_chat_login.removeClass('show').addClass('hide');
-           $form_chat_login[0].reset();
-           $submit_chat_login.attr('disabled', false);
-           $guestLogin.attr('disabled', false);
-           $chatLoginModal.modal();
-           $name.focus();
-       });
-       $form_chat_login.submit(function (e) {
-           userId = $email.val();
-           $submit_chat_login.attr('disabled', true);
-           $submit_chat_login.html('Initiating chat...');
-           $error_chat_login.removeClass('show').addClass('hide');
-           $error_chat_login.html("");
-           var formData = $(this).serialize();
-           var request = $.ajax({
-               url: "https://www.applozic.com/rest/ws/register/v2/admin",
-               type: "post",
-               data: formData,
-               success: function (data) {
-                   $submit_chat_login.attr('disabled', false);
-                   $submit_chat_login.html('Start chat');
-                   switch (data) {
-                       case "error":
-                           $error_chat_login.html("Oops! Looks like emailId is missing.</span>");
-                           $error_chat_login.removeClass('hide').addClass('show');
-                           break;
-                       case "alu01.shortpass":
-                           $error_chat_login.html("The password should be at least 6 characters.</span>");
-                           $error_chat_login.removeClass('hide').addClass('show');
-                           break;
-                       case "alu01.diffpass":
-                           $error_chat_login.html("The two passwords do not match.</span>");
-                           $error_chat_login.removeClass('hide').addClass('show');
-                           break;
-                       case "INCORRECT_PASSWORD":
-                           $error_chat_login.html("Oops! Looks like you have already registered. Please enter the correct password or <br><span> <a class='forgotPasswordModalLink' href='#'>Click here</a> if you forgot the password.</span>");
-                           $error_chat_login.removeClass('hide').addClass('show');
-                           break;
-                       default:
-                           initPlugin(userId, function (response) {
-                             if(response && response === 'success'){
-                               $submit_chat_login.attr('disabled', false);
-                               $submit_chat_login.html('Start chat');
-                               $error_chat_login.removeClass('show').addClass('hide');
-                               $form_chat_login[0].reset();
-                               $chatLoginModalLink.addClass('hide');
-                               $chatLoginModal.modal('hide');
-                               if (data === "USER_CREATED") {
-                                   //$("#mck-msg-new").trigger('click');
-                                   /*setTimeout(function() {
+    $chatLoginModalLink.click(function (e) {
+        $('.modal').modal('hide');
+        $error_chat_login.html("");
+        $error_chat_login.removeClass('show').addClass('hide');
+        $form_chat_login[0].reset();
+        $submit_chat_login.attr('disabled', false);
+        $guestLogin.attr('disabled', false);
+        $chatLoginModal.modal();
+        $name.focus();
+    });
+    $form_chat_login.submit(function (e) {
+        userId = $email.val();
+        $submit_chat_login.attr('disabled', true);
+        $submit_chat_login.html('Initiating chat...');
+        $error_chat_login.removeClass('show').addClass('hide');
+        $error_chat_login.html("");
+        var formData = $(this).serialize();
+        var request = $.ajax({
+            url: "https://www.applozic.com/rest/ws/register/v2/admin",
+            type: "post",
+            data: formData,
+            success: function (data) {
+                $submit_chat_login.attr('disabled', false);
+                $submit_chat_login.html('Start chat');
+                switch (data) {
+                    case "error":
+                        $error_chat_login.html("Oops! Looks like emailId is missing.</span>");
+                        $error_chat_login.removeClass('hide').addClass('show');
+                        break;
+                    case "alu01.shortpass":
+                        $error_chat_login.html("The password should be at least 6 characters.</span>");
+                        $error_chat_login.removeClass('hide').addClass('show');
+                        break;
+                    case "alu01.diffpass":
+                        $error_chat_login.html("The two passwords do not match.</span>");
+                        $error_chat_login.removeClass('hide').addClass('show');
+                        break;
+                    case "INCORRECT_PASSWORD":
+                        $error_chat_login.html("Oops! Looks like you have already registered. Please enter the correct password or <br><span> <a class='forgotPasswordModalLink' href='#'>Click here</a> if you forgot the password.</span>");
+                        $error_chat_login.removeClass('hide').addClass('show');
+                        break;
+                    default:
+                        initPlugin(userId, function (response) {
+                            if (response && response === 'success') {
+                                $submit_chat_login.attr('disabled', false);
+                                $submit_chat_login.html('Start chat');
+                                $error_chat_login.removeClass('show').addClass('hide');
+                                $form_chat_login[0].reset();
+                                $chatLoginModalLink.addClass('hide');
+                                $chatLoginModal.modal('hide');
+                                if (data === "USER_CREATED") {
+                                    //$("#mck-msg-new").trigger('click');
+                                    /*setTimeout(function() {
                                      $applozic.fn.applozic('addWelcomeMessage', {'sender': 'applozic', 'messageContent': welcomeMessage});
-                                   }, "fast");      */
-                                 }
+                                     }, "fast");      */
+                                }
 
-                                 $applozic.fn.applozic('getUserDetail', {callback: function(response) {
-                                           var users = response.data.users.length;
-                                           if (users == 0) {
-                                             //$applozic.fn.applozic('loadTab', 'applozic');
-                                             $applozic.fn.applozic('loadContextualTab', {'userId': 'applozic', 'topicId' : topicId});
-                                           } else {
-                                             $applozic.fn.applozic('loadTab', '');
-                                           }
+                                $applozic.fn.applozic('getUserDetail', {callback: function (response) {
+                                        if (response.status === 'success') {
+                                            var users = response.data.users.length;
+                                            if (users == 0) {
+                                                if (topicId) {
+                                                    $applozic.fn.applozic('loadContextualTab', {'userId': 'applozic', 'topicId': topicId});
+                                                } else {
+                                                    $applozic.fn.applozic('loadTab', 'applozic');
+                                                }
+                                            } else {
+                                                $applozic.fn.applozic('loadTab', '');
+                                            }
+                                        }
+                                    }
+                                });
+                            } else if (response && response.status === 'error' && response.errorMessage === 'INVALID PASSWORD') {
+                                window.location = "https://www.applozic.com/views/applozic/page/admin/dashboard.jsp";
+                            } else {
+                                if (response && response.errorMessage) {
+                                    alert(response.errorMessage);
+                                } else {
+                                    alert("There is some error. Please contact us at contact@applozic.com");
+                                }
+                            }
+                        });
+                }
+            },
+            error: function (data, status) {
+                $submit_chat_login.attr('disabled', false);
+                $submit_chat_login.html('Start chat');
+                $error_chat_login.html('Unable to process your request. Please try again.');
+                $error_chat_login.removeClass('hide').addClass('show');
+            }
+        });
+        return false;
+    });
+    $guestLogin.on('click', function () {
+        $guestLogin.attr('disabled', true);
+        $guestLogin.html('Initiating chat...');
+        var firstTimeUser = false;
+        if (!userId) {
+            if ($.cookie("mckUserId")) {
+                userId = $.cookie("mckUserId");
+            } else {
+                userId = getRandomId();
+                firstTimeUser = true;
+                $.cookie("mckUserId", userId);
+            }
+        }
+        initPlugin(userId, function (response) {
+            $guestLogin.html('Chat as guest');
+            $guestLogin.attr('disabled', false);
+            $chatLoginModalLink.addClass('hide');
+            $chatLoginModal.modal('hide');
 
-                                     }
-                                 });
-                             } else if(response && response.status === 'error' && response.errorMessage === 'INVALID PASSWORD'){
-                               window.location = "https://www.applozic.com/views/applozic/page/admin/dashboard.jsp";
-                             } else {
-                               if(response && response.errorMessage){
-                                 alert(response.errorMessage);
-                               } else{
-                                 alert("There is some error. Please contact us at contact@applozic.com");
-                               }
-                             }
-                           });
-                   }
-               },
-               error: function (data, status) {
-                   $submit_chat_login.attr('disabled', false);
-                   $submit_chat_login.html('Start chat');
-                   $error_chat_login.html('Unable to process your request. Please try again.');
-                   $error_chat_login.removeClass('hide').addClass('show');
-               }
-           });
-           return false;
-       });
-       $guestLogin.on('click', function () {
-           $guestLogin.attr('disabled', true);
-           $guestLogin.html('Initiating chat...');
-           var firstTimeUser = false;
-           if (!userId) {
-               if ($.cookie("mckUserId")) {
-                   userId = $.cookie("mckUserId");
-               } else {
-                   userId = getRandomId();
-                   firstTimeUser = true;
-                   $.cookie("mckUserId", userId);
-               }
-           }
-           initPlugin(userId, function (response) {
-               $guestLogin.html('Chat as guest');
-               $guestLogin.attr('disabled', false);
-               $chatLoginModalLink.addClass('hide');
-               $chatLoginModal.modal('hide');
+            $applozic.fn.applozic('getUserDetail', {callback: function (response) {
+                    if (response.status === 'success') {
+                        var users = response.data.users.length;
+                        if (users == 0) {
+                            if (topicId) {
+                                $applozic.fn.applozic('loadContextualTab', {'userId': 'applozic', 'topicId': topicId});
+                            } else {
+                                $applozic.fn.applozic('loadTab', 'applozic');
+                            }
+                        } else {
+                            $applozic.fn.applozic('loadTab', '');
+                        }
+                    }
+                }
+            });
 
-               $applozic.fn.applozic('getUserDetail', {callback: function(response) {
-                     if(response.status === 'success') {
-                         var users = response.data.users.length;
-                         if (users == 0) {
-                           //$applozic.fn.applozic('loadTab', 'applozic');
-                           $applozic.fn.applozic('loadContextualTab', {'userId': 'applozic', 'topicId' : topicId});
-                         } else {
-                           $applozic.fn.applozic('loadTab', '');
-                         }
-                     }
-                   }
-               });
-
-               if (firstTimeUser) {
-                   /*setTimeout(function() {
-                     $applozic.fn.applozic('addWelcomeMessage', {'sender': 'applozic', 'messageContent': welcomeMessage});
-                   }, "fast");*/
-               }
-           });
-       });
-   });
+            if (firstTimeUser) {
+                /*setTimeout(function() {
+                 $applozic.fn.applozic('addWelcomeMessage', {'sender': 'applozic', 'messageContent': welcomeMessage});
+                 }, "fast");*/
+            }
+        });
+    });
+});
 
 function getRandomId() {
     var text = "";
